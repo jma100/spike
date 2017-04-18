eb2579c930fd73312f8d051c2e51099a5a5dc411
ed1d1a8d <tong.t.wang@gmail.com>
2017-04-18 16:47:04 -0400

    Add request counter

--- docker/src/server.js
+++ docker/src/server.js
@@ -4,8 +4,15 @@ var os = require("os");
 var app = express();
 var hostname = os.hostname();
 
+var request_count = 0;
+
 app.get('/', function (req, res) {
-  res.send('<html><body>Hello from Node.js container ' + hostname + '</body></html>');
+    ++request_count;
+    ret = '<html><body>'
+        + 'Hello from Node.js container ' + hostname + '.'
+        + ' I have processed ' + request_count + ' requests since startup.'
+        + '</body></html>';
+    res.send(ret);
 });
 
 app.listen(80);
