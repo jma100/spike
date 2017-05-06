073b744765c7b5a4694d98f7e4b3efba274aa553
Jingwei Ma <Jingwei Ma>
2017-05-06 17:25:37 -0400

    License

--- hashingsend_test.go
+++ hashingsend_test.go
@@ -1,43 +1,42 @@
-package consistent
+package main
 
 import (
-	"../spike/consistent"
 	"fmt"
 	"log"
+	"stathat.com/c/consistent"
 )
 
-fmt.Println("hello")
-
 func main() {
+	servers := createNew()
+	packets := []string{"192.168.124.100/50272/81.209.179.69/80/6", "192.168.124.100/50270/81.209.179.69/80/6", "192.168.124.100/50268/81.209.179.69/80/6", "192.168.124.100/50266/81.209.179.69/80/6", "192.168.124.100/50264/81.209.179.69/80/6"}
+	for _, p := range packets {
+		server, err := servers.Get(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s => %s\n", p, server)
+	}
+	AddServ()
+	RemoveServ()
+
+}
+
+func createNew() {
 	servers := consistent.New()
 	servers.Add("king-cakes")
 	servers.Add("strawberry-habanero")
 	servers.Add("cheesy-fries")
-	packets := []string{"192.168.124.100/50272/81.209.179.69/80/6", "192.168.124.100/50270/81.209.179.69/80/6", "192.168.124.100/50268/81.209.179.69/80/6", "192.168.124.100/50266/81.209.179.69/80/6", "192.168.124.100/50264/81.209.179.69/80/6"}
-	for _, p := range packets {
-		server, err := servers.Get(p)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s => %s\n", p, packets)
-	}
+	return servers
 }
 
-func AddServ(servers, addserver) { //addserver is a list of server names(String)
+func AddServ(servers consistent, []addserver)([]updatedservers) { //addserver is a list of server names(String)
 	for i := 0; i < len(addserver); i++ {
 		servers.Add(addserver[i])
 	}
-	packets := []string{"192.168.124.100/50272/81.209.179.69/80/6", "192.168.124.100/50270/81.209.179.69/80/6", "192.168.124.100/50268/81.209.179.69/80/6", "192.168.124.100/50266/81.209.179.69/80/6", "192.168.124.100/50264/81.209.179.69/80/6"}
-	for _, p := range packets {
-		server, err := servers.Get(p)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s => %s\n", p, packets)
-	}
+	return servers
 }
 
-func RemoveServ(servers, rmvserver) { //rmvserver is a list of server names(String)
+func RemoveServ(servers consistent, []rmvserver)([]updatedservers) { //rmvserver is a list of server names(String)
 	for i := 0; i < len(rmvserver); i++ {
 		var inserver = false
 		for _, a := range servers {
@@ -49,12 +48,5 @@ func RemoveServ(servers, rmvserver) { //rmvserver is a list of server names(Stri
 			servers.Remove(rmvserver[i])
 		}
 	}
-	packets := []string{"192.168.124.100/50272/81.209.179.69/80/6", "192.168.124.100/50270/81.209.179.69/80/6", "192.168.124.100/50268/81.209.179.69/80/6", "192.168.124.100/50266/81.209.179.69/80/6", "192.168.124.100/50264/81.209.179.69/80/6"}
-	for _, p := range packets {
-		server, err := servers.Get(p)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s => %s\n", p, packets)
-	}
+	return servers
 }
